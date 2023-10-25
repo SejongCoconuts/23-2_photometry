6e53df6f73f650644437e7b89390947aad960221
SejongCoconuts <148928610+SejongCoconuts@users.noreply.github.com>
2023-10-25 12:22:31 +0900

    Update pipeline.py

--- pipe_master/pipeline.py
+++ pipe_master/pipeline.py
@@ -14,9 +14,9 @@ delt = 14
 
 
 
-f0 = pd.read_csv(wdir+'lightcurve.dat', delim_whitespace=True, header=None)
-f1 = pd.read_csv(wdir+'lightcurve_power.dat', delim_whitespace=True, header=None)
-f2 = pd.read_csv(wdir+'lightcurve_broken_power.dat', delim_whitespace=True, header=None)
+f0 = pd.read_csv(wdir+'lightcurve.dat',                delim_whitespace=True, header=None)
+f1 = pd.read_csv(wdir+'lightcurve_power.dat',          delim_whitespace=True, header=None)
+f2 = pd.read_csv(wdir+'lightcurve_broken_power.dat',   delim_whitespace=True, header=None)
 f3 = pd.read_csv(wdir+'lightcurve_smoothed_power.dat', delim_whitespace=True, header=None)
 
 
