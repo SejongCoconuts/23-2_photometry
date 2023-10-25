f5a5fc9412c12d7fd3374920454f2ab9d9fa404d
SejongCoconuts <148928610+SejongCoconuts@users.noreply.github.com>
2023-10-25 12:07:23 +0900

    Update FermiLC.py

--- pipe_master/FermiLC.py
+++ pipe_master/FermiLC.py
@@ -72,11 +72,12 @@ def PSD(delt, time, flux):
         fj.append((i)/(len(flux)*delt))
     xbar = flux - np.mean(flux)
     time = time - time.values[0]
-    dft_list = []
-    for i in range(len(flux)):
-        dft = (np.sum(xbar*np.cos(2*np.pi*fj[i]*time)))**2 + \
-            (np.sum(xbar*np.sin(2*np.pi*fj[i]*time)))**2
-        dft_list.append(dft)
+    # dft_list = []
+    # for i in range(len(flux)):
+    #     dft = (np.sum(xbar*np.cos(2*np.pi*fj[i]*time)))**2 + \
+    #         (np.sum(xbar*np.sin(2*np.pi*fj[i]*time)))**2
+    #     dft_list.append(dft)
+    dft = (np.sum(xbar*np.cos(2*np.pi*fj*time)))**2 + (np.sum(xbar*np.sin(2*np.pi*fj*time)))**2
     return fj[1:], dft_list[1:]
 
 def interpolation(x, y, Type):
