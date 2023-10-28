26dedfcf90fea25dd29dd04306897597b03e2cff
Megahyun <148929009+Megahyun@users.noreply.github.com>
2023-10-28 17:25:42 +0900

    Update pipeline.py

--- pipe_master/pipeline.py
+++ pipe_master/pipeline.py
@@ -1,14 +1,23 @@
-
-import os 
-import numpy as np
-import pandas as pd
-
 from tasks import * 
 
-# EDIT HERE ==========================================================
-wdir = ''
+# Read line 
 
-delt = 14
+readline.parse_and_bind("tab: complete")
+def path_completer(text, state):
+    return [x for x in os.listdir('.') if x.startswith(text)][state]
+readline.set_completer(path_completer)
+
+# Load data
+
+path_lc = input('Data File Name:')
+df = pd.read_csv(path_lc)[['Julian Date', 'Photon Flux [0.1-100 GeV](photons cm-2 s-1)', 'Photon Flux Error(photons cm-2 s-1)']]
+
+# Remove spaces and operation symbols in data
+
+df = df[df['Photon Flux [0.1-100 GeV](photons cm-2 s-1)'].str.strip() != '']
+df['Photon Flux [0.1-100 GeV](photons cm-2 s-1)'] = df['Photon Flux [0.1-100 GeV](photons cm-2 s-1)'].str.replace('>', '').str.replace('<', '')
+df.to_csv('test.csv', index=False)
+
 # ====================================================================
 
 path_lc_ori = wdir + 'lightcurve.dat'
