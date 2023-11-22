0011c8125fa1b900e6ca96344f0d15c580dd997e
Jaebeom2425 <148928955+Jaebeom2425@users.noreply.github.com>
2023-11-22 12:59:18 +0900

    Update pseudo.py

--- pipe_master/pseudo.py
+++ pipe_master/pseudo.py
@@ -27,6 +27,23 @@ def fit_PDF(flux):
  
   return lnmu, lnsig, data_pdf, pdf_light
 
+
+
+def PSD(delt, time, flux):
+    fj = []
+    for i in range(len(flux)):
+        fj.append((i)/(len(flux)*delt))
+    xbar = flux - np.mean(flux)
+    time = time - time.values[0]
+    dft_list = []
+    for i in range(len(flux)):
+        dft = (np.sum(xbar*np.cos(2*np.pi*fj[i]*time)))**2 + \
+            (np.sum(xbar*np.sin(2*np.pi*fj[i]*time)))**2
+        dft_list.append(dft)
+    return fj[1:], dft_list[1:]
+
+
+
 def calc_PSD():
   return PSD,freq
 
