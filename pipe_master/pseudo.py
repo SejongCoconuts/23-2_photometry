bb064baeb1b380ee714cb511faaeedbae4270c57
LoreAR <148928858+LoreAR@users.noreply.github.com>
2023-11-22 12:53:08 +0900

    Update pseudo.py

--- pipe_master/pseudo.py
+++ pipe_master/pseudo.py
@@ -3,8 +3,29 @@ def read_original_csv(file):
   pd.read_csv()
   return time,flux,error
 
-def fit_PDF():
-  return PDF
+def fit_PDF(flux):
+ 
+  # Fit a log-normal distribution to the data and estimate parameters
+  lnsig, loc, scale = lognorm.fit(flux, floc=0)
+  
+  # Calculate the mean (lnmu)
+  lnmu = loc - (lnsig**2) / 2
+
+  # Generate PDF
+  data_pdf = np.linspace(0, data.max(), 1000)
+  pdf_light = lognorm.pdf(data_pdf, lnsig, loc, scale)
+  
+  fig, g = plt.subplots(figsize=(10, 5))
+  fig.set_facecolor('white')
+  g.plot(data_pdf, pdf_light, 'r-', lw=2, label='log-normal distribution PDF')
+  g.hist(flux, alpha = 0.5, bins=30, density=True, color = 'b', label=r'Photon Flux', edgecolor = 'black')
+  g.set_ylabel('Probability Density Fuction',fontsize=15)
+  g.legend(loc ='best')
+  for spine in ['top', 'right','bottom','left']:
+      g.spines[spine].set_linewidth(2)
+  g.tick_params(labelsize=10,length=3,width=2)
+ 
+  return lnmu, lnsig, data_pdf, pdf_light
 
 def calc_PSD():
   return PSD,freq
