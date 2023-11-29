a94f1d2bf1885857734dcca9325adaad3cb9477f
jhodgo <148928849+jhodgo@users.noreply.github.com>
2023-11-29 10:34:46 +0900

    Update example.py

--- pipe_master/pipe_combined/example.py
+++ pipe_master/pipe_combined/example.py
@@ -1,1 +1,18 @@
+from pipe import *
+
+def analysis_LC(path_orig, 
+                model="SimplePL", 
+                niters=10, 
+                binning=10, 
+                interpolate=False, 
+                interpolate_method="Linear",
+               removal_threshold=0):
+
+  LC = Analysis_LC(path_orig)
+  LC.fit_PDF()
+  LC.plot_PDF()
+								 
+
+                  
+
 
